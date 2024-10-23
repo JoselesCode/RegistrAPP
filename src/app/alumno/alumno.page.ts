276bd773cbbf56436a2037d57005cd5a8c37b8d2
JoselesCode <jo.diazg@duocuc.cl>
2024-10-23 02:13:09 -0300

    capacitor parte 2

--- src/app/alumno/alumno.page.ts
+++ src/app/alumno/alumno.page.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { ServicesG } from '../services/services-g.service';
-import { Location } from '@angular/common'; // Importar Location
-import { Router } from '@angular/router'; // Importar Router
+import { Location } from '@angular/common';
+import { Router } from '@angular/router';
+import { BarcodeScanner } from '@capacitor-community/barcode-scanner'; // Importar BarcodeScanner
 
 @Component({
   selector: 'app-alumno',
@@ -12,15 +13,41 @@ export class AlumnoPage {
   usuario: string | null = '';
 
   constructor(private servicesG: ServicesG, private location: Location, private router: Router) {
-    this.usuario = this.servicesG.getUsuarioActual(); 
+    this.usuario = this.servicesG.getUsuarioActual();
   }
 
   volver() {
-    this.location.back(); // Regresar a la página anterior
+    this.location.back();
   }
 
   cerrarSesion() {
-    this.router.navigate(['/home']); // Redirigir al login
+    this.router.navigate(['/home']);
+  }
+
+  // Método para iniciar el escaneo
+  async startScan() {
+    try {
+      // Solicitar permiso de cámara
+      const status = await BarcodeScanner.checkPermission({ force: true });
+      if (status.granted) {
+        // Ocultar la interfaz de la app para ver el escáner
+        BarcodeScanner.hideBackground();
+
+        // Iniciar el escaneo
+        const result = await BarcodeScanner.startScan();
+
+        // Verificar si hay contenido escaneado
+        if (result.hasContent) {
+          alert('Código QR detectado: ' + result.content); // Mostrar el contenido escaneado
+        } else {
+          alert('No se detectó ningún código QR');
+        }
+
+      } else {
+        alert('Permiso de cámara denegado');
+      }
+    } catch (e) {
+      console.error('Error al escanear:', e);
+    }
   }
 }
-
