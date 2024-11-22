0007bc8184e60c16cf86edde058120d7cb2da71e
JoselesCode <jo.diazg@duocuc.cl>
2024-11-21 22:13:47 -0300

    22-11 avance 1.2

--- src/app/informacion/informacion.page.ts
+++ src/app/informacion/informacion.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-informacion',
   templateUrl: './informacion.page.html',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InformacionPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+  ) { }
 
   ngOnInit() {
   }
 
+
+
+  volver() {
+    this.router.navigate(['/home']);
+  }
 }
